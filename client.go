4741160f9f8c8f72f84dcaabb33e9d2ceb3246ce
Amy Fung <af1@wellesley.edu>
2023-02-21 23:54:52 -0500

    Closed c

--- client.go
+++ client.go
@@ -1,14 +1,16 @@
 //used tutorial here: https://www.linode.com/docs/guides/developing-udp-and-tcp-clients-and-servers-in-go/
 
 package main
+
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"net"
 	"os"
 	"strings"
-	"log"
 )
+
 func main() {
 	args := os.Args
 	if len(args) <= 1 {
@@ -27,9 +29,9 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Fprintf(c, txt + "\n") //print connection and your text
+		fmt.Fprintf(c, txt+"\n")                      //print connection and your text
 		msg, _ := bufio.NewReader(c).ReadString('\n') //read what the server sends you
-		fmt.Print("->: " + msg) //print out the server's message
+		fmt.Print("->: " + msg)                       //print out the server's message
 		if strings.TrimSpace(string(txt)) == "STOP" { //if the user enters stop...
 			fmt.Println("TCP client now exiting. Goodbye!")
 			c.Close() //close connection
