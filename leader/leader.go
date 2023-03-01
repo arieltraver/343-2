87417c942368d308c30d6648b407a78cba369ec1
arieltraver <arieltraver@users.noreply.github.com>
2023-03-01 14:55:12 -0500

    workign

--- leader/leader.go
+++ leader/leader.go
@@ -8,6 +8,9 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sync"
+	"io"
+	"math"
 )
 
 var count = 0
@@ -63,4 +66,45 @@ func main() {
 			count++
 		}
 	}
+}
+
+func splitAndSend(directory string, numHosts int) {
+	fps, err := os.ReadDir(directory)
+	if err != nil {
+		log.Fatal(err)
+	}
+	file, err := os.Open(fps[0].Name())
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	fileInfo, err := file.Stat() // get file stats
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileSize := fileInfo.Size() // get file size
+	chunkSize := int(fileSize) / numHosts //size of chunk per each host
+	//chunkSize += 1
+
+	buffers := make([][]byte, numHosts)
+	for i := 0; i < numHosts; i++ {
+		partSize := int(math.Min(float64(chunkSize), float64(fileSize-int64(i*chunkSize))))
+		buff := make([]byte, partSize)
+		buffers[i] = buff
+	}
+
+	for i, buffer := range(buffers) {
+		bytesRead, err := file.Read(buffer)
+		if err != nil && err != io.EOF {
+			log.Fatal(err)
+		}
+		fmt.Println("bytes read:", bytesRead)
+		if err != io.EOF {
+			fmt.Println("reached end of file, chunks read:", i)
+			break;
+		}
+	}
+
+
 }
