1802d202170fc411cdbbd337bf0159052f0d5ab4
arieltraver <arieltraver@users.noreply.github.com>
2023-03-01 22:50:17 -0500

    plans written comments

--- leader/leader.go
+++ leader/leader.go
@@ -16,8 +16,15 @@ import (
 var count = 0
 
 func handleConnection(c net.Conn) {
+	//first: for loop waits around for a 'ready'
+	//next: if it gets a ready, check if any chunks need processing
+	//		if not, tell worker 'done' and close
+	//next: for loop performs the handshake
+	//next: for loop waits around for a string to be written by the worker
+	//next: interpret the string (bufio readlines could be useful)
+	//next: write the string into the global data structure
 	for {
-		netData, err := bufio.NewReader(c).ReadString('\n')
+		netData, err := bufio.NewReader(c).ReadString('\n') 
 		if err != nil {
 			log.Println(err) //prints to standard error
 			return
@@ -37,6 +44,18 @@ func handleConnection(c net.Conn) {
 }
 
 func main() {
+	
+	//perform the file chunk division
+	//create a global map data structure
+	//loop which waits for N hosts to connect
+	// --- N could be specified as a command line argument
+	// --- pass a pointer to the buffer array (with mutex) to each connection
+	// --- also need to pass a pointer to the global map (with mutex) to each connection
+	// use sync.waitgroup to wait for all threads to complete
+	// once all threads are complete, write the global map to a file
+	// end
+
+	
 	arguments := os.Args
 	if len(arguments) <= 1 {
 		fmt.Println("Usage: 'leader host directory'")
@@ -74,8 +93,6 @@ func main() {
   - Reads a file into chunks and saves these chunks in memory.
     this is gross bc you are going to need to request stack space.
     Returns a 3d byte array. each host has one array of bytes, and one array which just contains status
-
-*
 */
 func readAndSplit(directory string, numHosts int) *[][][]byte {
 	fps, err := os.ReadDir(directory)
@@ -92,42 +109,12 @@ func readAndSplit(directory string, numHosts int) *[][][]byte {
 	if err != nil {
 		log.Fatal(err)
 	}
-<<<<<<< HEAD
 	fileSize := fileInfo.Size() // get file size
 	chunkSize := int(fileSize) / numHosts + 1 //size of chunk per each host
 	fmt.Println("chunksize is", chunkSize)
-=======
-
-	fileSize := fileInfo.Size()                                        // get file size
-	chunkSize := int(math.Ceil(float64(fileSize) / float64(numHosts))) //size of chunk per each host
-	fmt.Println("chunksize is", chunkSize, "| filesize is", fileSize)
->>>>>>> 6b9acb749cd4c88d9cde1baac925f64c7f4f6047
 
 	buffers := make([][][]byte, numHosts)
-	read := 0
 	for i := 0; i < numHosts; i++ {
-<<<<<<< HEAD
-		//partSize := int(math.Min(float64(chunkSize), float64(fileSize-int64(i*chunkSize)))) //limits size to remaining
-		pair := make([][]byte, 2)
-		pair[0] = []byte {0}
-		buff := make([]byte, chunkSize)
-		pair[1] = buff
-		buffers[i] = pair //for each host, chunk and status
-	}
-
-	for i, pair := range(buffers) {
-		buffer := pair[1] //get the buffer, not the status
-		fmt.Println("length of buffer:", len(buffer))
-		bytesRead, err := file.Read(buffer) //read the length of buffer from file
-		if err != nil && err != io.EOF {
-			log.Fatal(err)
-		} else if err == io.EOF {
-			fmt.Println("reached end of file, chunks read:", i)
-			fmt.Println(buffer[len(buffer)-1]) //see if this is full
-			break;
-		}
-		fmt.Println("bytes read:", bytesRead)
-=======
 		//partSize := int(math.Min(float64(chunkSize), float64(fileSize-int64(i*chunkSize))))
 		buff := make([]byte, chunkSize)
 		fmt.Println("length of buffer:", len(buff))
@@ -142,11 +129,8 @@ func readAndSplit(directory string, numHosts int) *[][][]byte {
 				log.Fatal(err)
 			}
 		}
-		read += bytesRead
 		fmt.Println("bytes read:", bytesRead)
 		buffers[i] = [][]byte{{0}, buff}
->>>>>>> 6b9acb749cd4c88d9cde1baac925f64c7f4f6047
 	}
-	fmt.Println("total bytes read:", read)
 	return &buffers
 }
